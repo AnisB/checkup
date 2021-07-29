fba71f5ae710b36e808469cbcea31dd435211882
abenyoub <Benyoub.anis@gmail.com>
2021-07-30 00:26:54 +0200

    finish to test the book allocator

--- applications/allocator_tester.cpp
+++ applications/allocator_tester.cpp
@@ -2,6 +2,7 @@
 #include <bento_base/security.h>
 #include <bento_memory/common.h>
 #include <bento_memory/page_allocator.h>
+#include <bento_memory/book_allocator.h>
 
 struct TByte4
 {
@@ -11,6 +12,11 @@ struct TByte4
 struct TByte8
 {
 	double data;
+};
+
+struct TByte12
+{
+	int data[3];
 };
 
 struct TByte16
@@ -23,7 +29,7 @@ struct TByte32
 	float data[8];
 };
 
-int main()
+void test_page_allocator()
 {
 	// Create an allocator that allocates 16 bytes per chunk
 	bento::PageAllocator pageAllocator;
@@ -101,10 +107,148 @@ int main()
 		// Free all the data (top to bottom this time)
 		for (int32_t chunkIdx = 63; chunkIdx >= 0; --chunkIdx)
 			bento::make_delete<TByte16>(pageAllocator, c[chunkIdx]);
-		
+
 		// Make sure everything was freed
 		assert(pageAllocator.usage_flags() == 0x0000000000);
 	}
+}
+
+void test_book_allocator()
+{
+	bento::BookAllocator bookAllocator;
+	bookAllocator.initialize(4, 16);
+
+	// Grab the 4 pages for checks
+	bento::PageAllocator& page0 = bookAllocator.get_page_allocator(0);
+	bento::PageAllocator& page1 = bookAllocator.get_page_allocator(1);
+	bento::PageAllocator& page2 = bookAllocator.get_page_allocator(2);
+	bento::PageAllocator& page3 = bookAllocator.get_page_allocator(3);
+
+	// Test that the allocations fall into the right page (if can)
+	{
+		TByte4* byte4 = bento::make_new<TByte4>(bookAllocator);
+		assert(byte4 != nullptr);
+		assert(page0.usage_flags() == 0x00000001);
+		bento::make_delete<TByte4>(bookAllocator, byte4);
+
+		TByte8* byte8 = bento::make_new<TByte8>(bookAllocator);
+		assert(byte8 != nullptr);
+		assert(page1.usage_flags() == 0x00000001);
+		bento::make_delete<TByte8>(bookAllocator, byte8);
+
+		TByte16* byte16 = bento::make_new<TByte16>(bookAllocator);
+		assert(byte16 != nullptr);
+		assert(page3.usage_flags() == 0x00000001);
+		bento::make_delete<TByte16>(bookAllocator, byte16);
+
+		TByte32* byte32 = bento::make_new<TByte32>(bookAllocator);
+		assert(byte32 == nullptr);
+		assert(page0.usage_flags() == 0x00000000);
+		assert(page1.usage_flags() == 0x00000000);
+		assert(page2.usage_flags() == 0x00000000);
+		assert(page3.usage_flags() == 0x00000000);
+	}
+
+	// Test that the allocations fall into the next page even if chunk too big
+	{
+		// Allocate the 64 chunks of the page
+		TByte4* c[64];
+		for (uint32_t chunkIdx = 0; chunkIdx < 64; ++chunkIdx)
+		{
+			// Allocate the first three chunks
+			c[chunkIdx] = bento::make_new<TByte4>(bookAllocator);
+			assert(c[chunkIdx] != nullptr);
+		}
+		assert(page0.is_full());
+
+		TByte4* extra = bento::make_new<TByte4>(bookAllocator);
+		assert(extra != nullptr);
+		assert(page0.is_full());
+		assert(page1.usage_flags() == 0x00000001);
+
+		// Free all the data (top to bottom this time)
+		for (int32_t chunkIdx = 63; chunkIdx >= 0; --chunkIdx)
+			bento::make_delete<TByte4>(bookAllocator, c[chunkIdx]);
+		bento::make_delete<TByte4>(bookAllocator, extra);
+
+		// Make sure everything was freed
+		assert(page0.usage_flags() == 0x0000000000);
+		assert(page1.usage_flags() == 0x0000000000);
+		assert(page2.usage_flags() == 0x0000000000);
+		assert(page3.usage_flags() == 0x0000000000);
+	}
+
+	// Test that if all pages are full allocation fails
+	{
+		// Allocate the 64 chunks of the page
+		TByte4* c4[64];
+		for (uint32_t chunkIdx = 0; chunkIdx < 64; ++chunkIdx)
+		{
+			// Allocate the first three chunks
+			c4[chunkIdx] = bento::make_new<TByte4>(bookAllocator);
+			assert(c4[chunkIdx] != nullptr);
+		}
+		assert(page0.is_full());
+
+		// Allocate the 64 chunks of the page
+		TByte8* c8[64];
+		for (uint32_t chunkIdx = 0; chunkIdx < 64; ++chunkIdx)
+		{
+			// Allocate the first three chunks
+			c8[chunkIdx] = bento::make_new<TByte8>(bookAllocator);
+			assert(c8[chunkIdx] != nullptr);
+		}
+		assert(page1.is_full());
+
+		// Allocate the 64 chunks of the page
+		TByte12* c12[64];
+		for (uint32_t chunkIdx = 0; chunkIdx < 64; ++chunkIdx)
+		{
+			// Allocate the first three chunks
+			c12[chunkIdx] = bento::make_new<TByte12>(bookAllocator);
+			assert(c12[chunkIdx] != nullptr);
+		}
+		assert(page2.is_full());
+
+		// Allocate the 64 chunks of the page
+		TByte16* c16[64];
+		for (uint32_t chunkIdx = 0; chunkIdx < 64; ++chunkIdx)
+		{
+			// Allocate the first three chunks
+			c16[chunkIdx] = bento::make_new<TByte16>(bookAllocator);
+			assert(c16[chunkIdx] != nullptr);
+		}
+		assert(page3.is_full());
+
+		// Do the smallest too much allocation
+		TByte4* tooMuch = bento::make_new<TByte4>(bookAllocator);
+		assert(tooMuch == nullptr);
+
+		// Free all the data (top to bottom this time)
+		for (int32_t chunkIdx = 63; chunkIdx >= 0; --chunkIdx)
+			bento::make_delete<TByte16>(bookAllocator, c16[chunkIdx]);
+		for (int32_t chunkIdx = 63; chunkIdx >= 0; --chunkIdx)
+			bento::make_delete<TByte12>(bookAllocator, c12[chunkIdx]);
+		for (int32_t chunkIdx = 63; chunkIdx >= 0; --chunkIdx)
+			bento::make_delete<TByte8>(bookAllocator, c8[chunkIdx]);
+		for (int32_t chunkIdx = 63; chunkIdx >= 0; --chunkIdx)
+			bento::make_delete<TByte4>(bookAllocator, c4[chunkIdx]);
+
+		// Make sure everything was freed
+		assert(page0.usage_flags() == 0x0000000000);
+		assert(page1.usage_flags() == 0x0000000000);
+		assert(page2.usage_flags() == 0x0000000000);
+		assert(page3.usage_flags() == 0x0000000000);
+	}
+}
+
+int main()
+{
+	// Run the page allocator tests
+	test_page_allocator();
+
+	// Run the book allocator tests
+	test_book_allocator();
 
 	// If we got here, everything is fine
 	return 0;
